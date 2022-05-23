0d50b459f7ac4e7843fe4e624131d4c4ba31572f
Logovskii Dmitrii <still65@yandex-team.ru>
2022-05-23 22:33:54 +0300

    unified tests run

--- tests/vdsina_test.py
+++ tests/vdsina_test.py
@@ -1,14 +1,28 @@
 import os
 
+import pytest
 import vcr
 
 from vdsinadriver import VdsinaDriver
 
 
+@pytest.fixture()
+def vdsina_key() -> str:
+    key = os.getenv("VDSINA_TOKEN")
+    if key is None:
+        pytest.exit("set up VDSINA_TOKEN environment variable")
+    return str(key)
+
+
+@pytest.fixture()
+def compute_conn(vdsina_key):
+    conn = VdsinaDriver(key=vdsina_key)
+    return conn
+
+
 @vcr.use_cassette("./tests/fixtures/list_key_pairs.yaml", filter_headers=["Authorization"])
-def test_list_key_pairs():
-    conn = VdsinaDriver(key=os.getenv("VDSINA_TOKEN"))
-    keys = conn.list_key_pairs()
+def test_list_key_pairs(compute_conn):
+    keys = compute_conn.list_key_pairs()
     key = keys.pop()
     assert key.name == "x200s"
     assert key.fingerprint == "ca:28:dc:6e:9e:72:48:d3:bc:73:76:19:d2:5f:c3:e8"
